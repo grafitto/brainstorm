c30815abe8ce79559084d82979cfa000096698ef
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-12-14 14:35:48 +0300

    Created the Instruction Base Class

---
+++ BrainStorm/Base/Instruction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainStorm
+{
+    abstract class Instruction
+    {
+        private int cycles;
+        public int Cycles
+        {
+            get{ return cycles; }
+            set{ Cycles = cycles; }
+        }
+        /**
+         * This is where the instruction executes itself using the visitor technique
+         **/
+        abstract public void execute(Processor processor);
+    }
+}
