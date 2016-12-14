a858ca95c3144c12415997d1fb069b54d6930bbf
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-12-14 19:18:04 +0300

    Created .gitignore file

---
+++ BrainStorm/Test.cs
@@ -0,0 +1,23 @@
+﻿using BrainStorm.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace brainstorm
+{
+    class MIPSRegisters : RegisterSet
+    {
+        public MIPSRegisters(int size) : base(size)
+        {
+        }
+    }
+    class Test
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Testing");
+        }
+    }
+}
