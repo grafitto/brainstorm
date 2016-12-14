415215cc0e91107278e87d282f60086dda4d7586
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-12-14 14:35:24 +0300

    Created the base Processor Class

---
+++ BrainStorm/Base/Processor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainStorm
+{
+    abstract class Processor
+    {
+        private int cycles;
+
+        public void tick(Instruction instruction)
+        {
+            this.cycles += instruction.Cycles;
+        }
+    }
+}
