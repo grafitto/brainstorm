1e80aaca1c2a37abc701cf8d2436c5bcf2aa36f0
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-12-14 14:34:45 +0300

    Added Exception classes

---
+++ BrainStorm/Exceptions/BrainStormException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainStorm.Exceptions
+{
+    abstract class BrainStormException : Exception
+    {
+        public BrainStormException() { }
+        public BrainStormException(string message) : base(message) { }
+        public BrainStormException(string message, Exception inner) : base(message, inner) { }
+    }
+}
---
+++ BrainStorm/Exceptions/IndexNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainStorm.Exceptions
+{
+    class IndexNotFoundException : BrainStormException
+    {
+        public IndexNotFoundException() { }
+        public IndexNotFoundException(string message) : base(message) { }
+        public IndexNotFoundException(string message, Exception inner) : base(message, inner) { }
+    }
+}
