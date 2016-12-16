aa85b206104e00506b840463a47c84e03849c186
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-12-16 12:27:43 +0300

    Added AssemblerException and RuntimeSimException classes which extends the base class SimulationException class

---
+++ BrainStorm/Exceptions/AssemblerException.cs
@@ -0,0 +1,16 @@
+﻿using BrainStorm.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace brainstorm.Exceptions
+{
+    class AssemblerException : SimulationException
+    {
+        public AssemblerException() { }
+        public AssemblerException(string message) : base(message) { }
+        public AssemblerException(string message, Exception inner) : base(message, inner) { }
+    }
+}
