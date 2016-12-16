02d23ec3e68ae0d0f37b4a515c10f2b4c3095e65
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-12-16 12:26:22 +0300

    Changed the Exception System. Added SimulationException as the base class

---
+++ BrainStorm/Exceptions/SimulationException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainStorm.Exceptions
+{
+    abstract class SimulationException : Exception
+    {
+        public SimulationException() { }
+        public SimulationException(string message) : base(message) { }
+        public SimulationException(string message, Exception inner) : base(message, inner) { }
+    }
+}
