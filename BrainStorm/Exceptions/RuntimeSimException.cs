0ebb897cf01c343e4a18fa8ae62836f10f4f9df5
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-12-16 12:28:13 +0300

    Added RuntimeSimException class for all Runtime activities

---
+++ BrainStorm/Exceptions/RuntimeSimException.cs
@@ -0,0 +1,16 @@
+﻿using BrainStorm.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace brainstorm.Exceptions
+{
+    class RuntimeSimException : SimulationException
+    {
+        public RuntimeSimException() { }
+        public RuntimeSimException(string message) : base(message) { }
+        public RuntimeSimException(string message, Exception inner) : base(message, inner) { }
+    }
+}
