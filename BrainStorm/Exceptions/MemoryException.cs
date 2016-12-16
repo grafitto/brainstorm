7c55ca57b97d351e976e3b3df107743867ee20b3
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-12-16 12:31:08 +0300

    Added the three classes ProcessorException, MemoryException and RegisterException classes for their respective functions

---
+++ BrainStorm/Exceptions/MemoryException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace brainstorm.Exceptions
+{
+    class MemoryException : RuntimeSimException
+    {
+        public MemoryException() { }
+        public MemoryException(string message) : base(message) { }
+        public MemoryException(string message, Exception inner) : base(message, inner) { }
+    }
+}
---
+++ BrainStorm/Exceptions/RegisterException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace brainstorm.Exceptions
+{
+    class RegisterException : MemoryException
+    {
+        public RegisterException() { }
+        public RegisterException(string message) : base(message) { }
+        public RegisterException(string message, Exception inner) : base(message, inner) { }
+    }
+}
