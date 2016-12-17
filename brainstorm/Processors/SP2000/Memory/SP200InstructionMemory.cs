9a716097359f12f9658248f3e569b778dccc894a
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-12-17 20:02:13 +0300

    SP2000 Memory
    Added SP2000 Memory classes

---
+++ brainstorm/Processors/SP2000/Memory/SP200InstructionMemory.cs
@@ -0,0 +1,13 @@
+﻿using BrainStorm.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainStorm.Processors.SP2000.Memory
+{
+    class SP200InstructionMemory : InstructionMemory
+    {
+    }
+}
