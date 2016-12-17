f389e32c302686eda698d7d9a996ac49993de169
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-12-17 19:58:22 +0300

    Added getters and setters, changed cycles to public in Processor

--- BrainStorm/Base/Processor.cs
+++ BrainStorm/Base/Processor.cs
@@ -4,18 +4,42 @@ namespace BrainStorm.Base
 {
     abstract class Processor
     {
-        private int cycles;
+        public int cycles;
         private InstructionMemory program;
+
+        public InstructionMemory Program
+        {
+            get { return program; }
+            set { this.program = Program; }
+        }
+
         private DataMemory memory;
+
+        public DataMemory Memory
+        {
+            get { return memory; }
+            set { this.memory = Memory; }
+        }
+        private RegisterSet registers;
+
+        public RegisterSet Registers
+        {
+            get { return registers; }
+            set { this.registers = Registers; }
+        }
+
+        public Processor(InstructionMemory program, DataMemory memory, RegisterSet registers)
+        {
+            this.program = program;
+            this.memory = memory;
+            this.registers = registers;
+        }
 
         /// <summary>
         /// The tick method keeps track of the total cycles since the program started
         /// </summary>
         /// <param name="instruction"></param>
-        
-        public void tick(Instruction instruction)
-        {
-            this.cycles += instruction.Cycles;
-        }
+
+        public virtual void tick(Instruction instruction) { }
     }
 }
