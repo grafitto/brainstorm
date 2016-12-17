59d021ee266377f7900e94b1d6ce2acb5bf67ceb
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-12-17 20:03:14 +0300

    Testing
    Some testing here and there

--- BrainStorm/Test.cs
+++ BrainStorm/Test.cs
@@ -1,30 +1,34 @@
-﻿using BrainStorm.Base;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BrainStorm.Processors.SP2000.Memory;
+using BrainStorm.Processor.SP2000.Instructions;
+using BrainStorm.Processors.SP2000.Processor;
+using BrainStorm.Processors.SP2000.Instructions;
 
-namespace brainstorm
+namespace BrainStorm
 {
-    class MIPSRegisters : RegisterSet
-    {
-        /// <summary>
-        /// This initializes the sie of the register set
-        /// </summary>
-        /// <param name="size"></param>
-        public MIPSRegisters(int size) : base(size)
-        {
-        }
-    }
+
     class Test
     {
         public static void Main(string[] args)
         {
-            MIPSRegisters reg = new MIPSRegisters(6);
-            reg.MakeRegister(new string[] { "$r1" }, 0);
-            reg.StoreToName("$r1", 45);
+            SP2000Registers registers = new SP2000Registers(6);
+            registers.MakeRegister(new string[] { "$t0" }, 0);
+            registers.MakeRegister(new string[] { "$t1" }, 1);
+            registers.MakeRegister(new string[] { "$t2" }, 2);
 
+            registers.StoreToName("$t0", 2);
+            registers.StoreToName("$t1", 0);
+            registers.StoreToName("$t2", 12);
+
+            SP200InstructionMemory program = new SP200InstructionMemory();
+            SP2000DataMemory memory = new SP2000DataMemory();
+
+            SP2000Processor chip = new SP2000Processor(program, memory, registers);
+
+            AddInstruction inst = new AddInstruction("add $t1, $t0, $t2", "$t1", "$t0", "$t2");
+
+            chip.tick(inst);
+
+            registers.ShowRegisters();
         }
     }
 }
