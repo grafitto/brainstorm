951b1f398670225a0a8515ca975ef804d0aad463
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-12-17 20:01:43 +0300

    SP2000 Registers

---
+++ brainstorm/Processors/SP2000/Memory/SP2000Registers.cs
@@ -0,0 +1,16 @@
+﻿using BrainStorm.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainStorm.Processors.SP2000.Memory
+{
+    class SP2000Registers : RegisterSet
+    {
+        public SP2000Registers(int size) : base(size)
+        {
+        }
+    }
+}
