ce82f1c030d5e88f2d7d5fc455d5a441ac0ed9fb
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-12-17 20:03:25 +0300

    Visual Studio Crap

--- BrainStorm/Processors/SP2000/Processor/SP2000Processor.cs
+++ BrainStorm/Processors/SP2000/Processor/SP2000Processor.cs
@@ -1,6 +1,19 @@
-﻿namespace brainstorm.Processors.SP2000.Processor
+﻿using BrainStorm.Processors.SP2000.Memory;
+using BrainStorm.Processor.SP2000.Instructions;
+using BrainStorm.Processors.SP2000.Instructions;
+
+namespace BrainStorm.Processors.SP2000.Processor
 {
-    class SP2000Processor : BrainStorm.Processor
+    class SP2000Processor : Base.Processor
     {
+        public SP2000Processor(SP200InstructionMemory program, SP2000DataMemory memory, SP2000Registers registers) : base(program, memory, registers)
+        {
+
+        }
+        public void tick(SP2000Instruction instruction)
+        {
+            base.tick(instruction);
+            ((AddInstruction)instruction).execute(this);
+        }
     }
 }
