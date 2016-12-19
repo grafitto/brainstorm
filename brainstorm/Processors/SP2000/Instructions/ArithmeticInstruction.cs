c42cb31e2ecd84815db459167097b0c825351f7d
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-12-19 10:35:25 +0300

    Removed unnecesary using statements

--- brainstorm/Processors/SP2000/Instructions/ArithmeticInstruction.cs
+++ brainstorm/Processors/SP2000/Instructions/ArithmeticInstruction.cs
@@ -1,9 +1,4 @@
-﻿using BrainStorm.Processor.SP2000.Instructions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace BrainStorm.Processors.SP2000.Instructions
 {
