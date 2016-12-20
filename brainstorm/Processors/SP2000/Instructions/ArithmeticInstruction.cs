0893ea779cada3a25a47790f07be8e5a1160e37a
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-12-20 14:17:33 +0300

    Added common attributes used by children classes

--- brainstorm/Processors/SP2000/Instructions/ArithmeticInstruction.cs
+++ brainstorm/Processors/SP2000/Instructions/ArithmeticInstruction.cs
@@ -5,9 +5,13 @@ namespace BrainStorm.Processors.SP2000.Instructions
     abstract class ArithmeticInstruction : ThreeOpInstruction
     {
         protected string destination;
+        protected string firstOperand;
+        protected string secondOperand;
         public ArithmeticInstruction(string instruction, string destination, string firstOperand, string secondOperand) : base(instruction, destination, firstOperand, secondOperand)
         {
             this.destination = destination;
+            this.firstOperand = firstOperand;
+            this.secondOperand = secondOperand;
         }
     }
 }
