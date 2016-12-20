8b1953aceb405146a1e71660634e43daef522783
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-12-20 14:18:57 +0300

    Added OR Instruction

---
+++ brainstorm/Processors/SP2000/Instructions/Bitwise/OriInstruction.cs
@@ -0,0 +1,50 @@
+﻿using brainstorm.Exceptions;
+using BrainStorm.Processors.SP2000.Instructions;
+using BrainStorm.Processors.SP2000.Memory;
+using BrainStorm.Processors.SP2000.Processor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace brainstorm.Processors.SP2000.Instructions
+{
+    class OriInstruction : ArithmeticInstruction
+    {
+        private int immidiate;
+        public OriInstruction(string instruction, string destination, string firstOperand, string secondOperand) : base(instruction, destination, firstOperand, secondOperand)
+        {
+            try
+            {
+                immidiate = int.Parse(secondOperand);
+            }catch(FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// This excecuted the instruction and changes the CPU register values
+        /// </summary>
+        /// <param name="processor"></param>
+        public override void execute(SP2000Processor processor)
+        {
+            SP2000Registers registers = (SP2000Registers)processor.Registers;
+            int result = 0;
+            try
+            {
+                int first = registers.FetchRegister(firstOperand);
+                result = first | immidiate;
+            }
+            catch (RegisterException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                registers.StoreToName(destination, result);
+            }
+        }
+    }
+}
