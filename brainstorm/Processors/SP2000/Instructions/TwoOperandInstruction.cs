4630f11fccbc37cefa56c05f4bd05070ab1d0094
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-12-20 14:20:48 +0300

    Added base class for 2 Operand Instructions

---
+++ brainstorm/Processors/SP2000/Instructions/TwoOperandInstruction.cs
@@ -0,0 +1,14 @@
+﻿using BrainStorm.Processor.SP2000.Instructions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace brainstorm.Processors.SP2000.Instructions
+{
+    class TwoOperandInstruction : SP2000Instruction
+    {
+        public TwoOperandInstruction(string instruction, string firstOperand, string secondOperand) : base(instruction){}
+    }
+}
