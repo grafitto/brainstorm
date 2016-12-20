d4ee65bdc26e235a3efbbcd407721a36964d3f05
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-12-20 14:19:43 +0300

    Added Immidiate XOR Instruction

---
+++ brainstorm/Processors/SP2000/Instructions/Bitwise/XoriInstruction.cs
@@ -0,0 +1,51 @@
+﻿using brainstorm.Exceptions;
+using BrainStorm.Processors.SP2000.Instructions;
+using BrainStorm.Processors.SP2000.Memory;
+using BrainStorm.Processors.SP2000.Processor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace brainstorm.Processors.SP2000.Instructions
+{
+    class XoriInstruction : ArithmeticInstruction
+    {
+        private int immidiate;
+        public XoriInstruction(string instruction, string destination, string firstOperand, string secondOperand) : base(instruction, destination, firstOperand, secondOperand)
+        {
+            try
+            {
+                immidiate = int.Parse(secondOperand);
+            }catch(FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// This excecuted the instruction and changes the CPU register values
+        /// </summary>
+        /// <param name="processor"></param>
+        public override void execute(SP2000Processor processor)
+        {
+            SP2000Registers registers = (SP2000Registers)processor.Registers;
+            int result = 0;
+            try
+            {
+                int first = registers.FetchRegister(firstOperand);
+  
+                result = first ^ immidiate;
+            }
+            catch (RegisterException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                registers.StoreToName(destination, result);
+            }
+        }
+    }
+}
