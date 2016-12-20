70dd33b994f9a7c1fca5bf415f68a083e387bdcf
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-12-20 14:20:27 +0300

    Added Shift Right Logic Instruction

---
+++ brainstorm/Processors/SP2000/Instructions/Bitwise/SrlInstruction.cs
@@ -0,0 +1,47 @@
+﻿using brainstorm.Exceptions;
+using BrainStorm.Processors.SP2000.Instructions;
+using BrainStorm.Processors.SP2000.Memory;
+using BrainStorm.Processors.SP2000.Processor;
+using System;
+
+namespace brainstorm.Processors.SP2000.Instructions.Bitwise
+{
+    class SrlInstruction : ArithmeticInstruction
+    {
+        private int immidiate;
+        public SrlInstruction(string instruction, string destination, string firstOperand, string secondOperand) : base(instruction, destination, firstOperand, secondOperand)
+        {
+            try
+            {
+                immidiate = int.Parse(secondOperand);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// This excecuted the instruction and changes the CPU register values
+        /// </summary>
+        /// <param name="processor"></param>
+        public override void execute(SP2000Processor processor)
+        {
+            SP2000Registers registers = (SP2000Registers)processor.Registers;
+            int result = 0;
+            try
+            {
+                int first = registers.FetchRegister(firstOperand);
+                result = first >> immidiate;
+            }
+            catch (RegisterException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                registers.StoreToName(destination, result);
+            }
+        }
+    }
+}
