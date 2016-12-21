a4084eb2778cc2e11d3c075da04455c1f801efb5
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-12-21 18:27:14 +0300

    Implimented SP2000 Core

---
+++ brainstorm/Processors/SP2000/Processor/SP2000Core.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BrainStorm.Base;
+using BrainStorm.Memory;
+using brainstorm.Base;
+using BrainStorm.Processor.SP2000.Instructions;
+
+namespace BrainStorm.Processors.SP2000.Processor
+{
+    class SP2000Core : Core
+    {
+        public SP2000Core(RegisterSet registers) : base(registers) { }
+
+    }
+}
