dbc15c243b2c1b63d923dbddf1d4cc4963144a35
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-12-21 18:25:41 +0300

    Created a core in a multicore system

---
+++ brainstorm/Base/Core.cs
@@ -0,0 +1,23 @@
+﻿using BrainStorm.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace brainstorm.Base
+{
+    abstract class Core
+    {
+        private RegisterSet registers;
+        public RegisterSet Registers
+        {
+            get { return this.registers; }
+            set { this.registers = Registers; }
+        }
+        public Core(RegisterSet registers)
+        {
+            this.registers = registers;
+        }
+    }
+}
