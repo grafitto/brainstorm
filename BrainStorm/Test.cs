be210372fc665bd2ea251fca27d107a52fb389b6
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-12-21 18:27:51 +0300

    Testing stuff

--- BrainStorm/Test.cs
+++ BrainStorm/Test.cs
@@ -1,11 +1,7 @@
 ﻿using BrainStorm.Processors.SP2000.Memory;
 using BrainStorm.Processors.SP2000.Processor;
-using brainstorm.Processors.SP2000.Instructions;
 using System;
-using brainstorm.Processors.SP2000.Instructions.Arithmetic;
-using BrainStorm.Processors.SP2000.Instructions;
-using BrainStorm.Base;
-using System.Collections.Generic;
+using brainstorm.Processors.SP2000.Instructions.Bitwise;
 
 namespace BrainStorm
 {
@@ -14,36 +10,23 @@ namespace BrainStorm
     {
         public static void Main(string[] args)
         {
-             SP2000Registers registers = new SP2000Registers(6);
-             registers.MakeRegister(new string[] { "$t0" }, 0);
-             registers.MakeRegister(new string[] { "$t1" }, 1);
-             registers.MakeRegister(new string[] { "$t2" }, 2);
+           SP2000Registers registers = new SP2000Registers();
 
-             registers.StoreToName("$t0", 4);
-             registers.StoreToName("$t1", 0);
-             registers.StoreToName("$t2", 13);
+           registers.StoreToName("$s2", 4);
+           registers.StoreToName("$s0", 60);
+           registers.StoreToName("$s1", 2);
+           //registers.ShowRegisters();
 
-             SP200InstructionMemory program = new SP200InstructionMemory();
-             SP2000DataMemory memory = new SP2000DataMemory();
+           
+            SP200InstructionMemory program = new SP200InstructionMemory();
+            SP2000DataMemory memory = new SP2000DataMemory();
 
-             SP2000Processor chip = new SP2000Processor(program, memory, registers);
+            SP2000Processor chip = new SP2000Processor(program, memory, registers);
 
-             MoveInstruction move = new MoveInstruction("move $t1, $t0", "$t1", "$t2");
-            AddInstruction add = new AddInstruction("add $t0, $t1, $t2", "$t0", "$t1", "$t2");
-            SubInstruction sub = new SubInstruction("sub $t0, $t1, $t2", "$t0", "$t1", "$t2");
+            SrlInstruction add = new SrlInstruction("srl $s2, $s0, 2", "$s2", "$s0", "2");
 
-            program.push(move, 0);
-            program.push(add, 2);
-            program.push(sub, 1);
-
-            foreach(KeyValuePair<int, Instruction> inst in program)
-            {
-                Console.WriteLine(inst.Key.ToString() + " => " + inst.Value.ToString());
-            }
-
-             //chip.tick(inst);
-
-            // registers.ShowRegisters();
+            chip.tick(add);
+            registers.ShowRegisters();
 
             /*  int value = 1088;
 
