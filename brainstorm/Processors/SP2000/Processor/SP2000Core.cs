fae2f8eecdd70443ddd885a9a3c4bda1395e44fa
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-12-23 12:00:46 +0300

    Added execute method in a core

--- brainstorm/Processors/SP2000/Processor/SP2000Core.cs
+++ brainstorm/Processors/SP2000/Processor/SP2000Core.cs
@@ -7,12 +7,51 @@ using BrainStorm.Base;
 using BrainStorm.Memory;
 using brainstorm.Base;
 using BrainStorm.Processor.SP2000.Instructions;
+using BrainStorm.Processors.SP2000.Memory;
 
 namespace BrainStorm.Processors.SP2000.Processor
 {
     class SP2000Core : Core
     {
-        public SP2000Core(RegisterSet registers) : base(registers) { }
+        private RegisterSet registers;
+        public new RegisterSet Registers
+        {
+            get { return registers; }
+        }
+        public SP2000Core(InstructionMemory program) : base(program)
+        {
+            registers = new SP2000Registers();
+        }
 
+        /// <summary>
+        /// Executes an instruction found in the specified address if any, else fetches the from PC
+        /// </summary>
+        /// <param name="address"></param>
+        public override void execute(int? address = null)
+        {
+            if(address != null)
+            {
+                SP2000Instruction current = this.getInstruction((int)address);
+                current.execute(this);
+            }
+            else
+            {
+                Register register = registers.FetchRegister("PC");
+                SP2000Instruction current = this.getInstruction(register.Value);
+                current.execute(this);
+
+            }
+            //Registers.ShowRegisters();
+        }
+
+        /// <summary>
+        /// Gets an instruction in an address
+        /// </summary>
+        /// <param name="address"></param>
+        /// <returns></returns>
+        private SP2000Instruction getInstruction(int address)
+        {
+            return (SP2000Instruction)Program.pop((int)address);
+        }
     }
 }
