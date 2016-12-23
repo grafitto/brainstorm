5d3455b4a2c833cad04c0d380caa6d49c7f8f74b
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-12-23 12:02:21 +0300

    Added program in the core

--- brainstorm/Base/Core.cs
+++ brainstorm/Base/Core.cs
@@ -1,4 +1,5 @@
 ﻿using BrainStorm.Base;
+using BrainStorm.Memory;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,9 +16,17 @@ namespace brainstorm.Base
             get { return this.registers; }
             set { this.registers = Registers; }
         }
-        public Core(RegisterSet registers)
+        private InstructionMemory program;
+        public InstructionMemory Program
+        {
+            get { return program; }
+            set { program = Program; }
+        }
+        public Core(InstructionMemory program)
         {
             this.registers = registers;
+            this.program = program;
         }
+        public abstract void execute(int? address = null);
     }
 }
