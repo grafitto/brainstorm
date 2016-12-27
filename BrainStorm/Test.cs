7fd7470d8aa64e9decc0d7299bc2efb08de4b420
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-12-27 21:30:52 +0300

    Tested the implimented functionalities

--- BrainStorm/Test.cs
+++ BrainStorm/Test.cs
@@ -2,6 +2,8 @@
 using BrainStorm.Processors.SP2000.Processor;
 using System;
 using brainstorm.Processors.SP2000.Instructions.Arithmetic;
+using brainstorm.Processors.SP2000.Instructions.Memory;
+using brainstorm.Processors.SP2000.Instructions;
 
 namespace BrainStorm
 {
@@ -13,12 +15,18 @@ namespace BrainStorm
            
             SP200InstructionMemory program = new SP200InstructionMemory();
             SP2000DataMemory memory = new SP2000DataMemory();
-            MoveInstruction add = new MoveInstruction("mov $v0, $v1", "$v0", "$v1");
-            program.push(add, 0);
+            LiInstruction li = new LiInstruction("li $v1, 8", "$v1", 8);
+            SubInstruction sub = new SubInstruction("sub $at, $v1, $v0", "$at", "$v1", "$v0");
+            AddiInstruction add = new AddiInstruction("addi $v0, $v1, 5", "$v0", "$v1", 5);
+            program.Push(li, 0);
+            program.Push(sub, 1);
+            program.Push(add, 2);
 
             SP2000Processor chip = new SP2000Processor(program, memory);
 
-            chip.tick();
+            chip.Tick();
+            chip.Tick();
+            chip.Tick();
             //registers.ShowRegisters();
 
             /*  int value = 1088;
