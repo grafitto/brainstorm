68c9b332eccce26d82d8744634c7c91add7277ac
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-12-29 23:00:35 +0300

    Added base class for one operand instructions

---
+++ brainstorm/Processors/SP2000/Instructions/OneOperandInstruction.cs
@@ -0,0 +1,14 @@
+﻿using BrainStorm.Processor.SP2000.Instructions;
+
+namespace brainstorm.Processors.SP2000.Instructions
+{
+    class OneOperandInstruction : SP2000Instruction
+    {
+        protected string firstOperand;
+
+        public OneOperandInstruction(string instruction, string firstOperand) : base(instruction)
+        {
+            this.firstOperand = firstOperand;
+        }
+    }
+}
