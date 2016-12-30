788154630dc00d5a5844a4ae6f7cfba9a690f272
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-12-30 16:12:15 +0300

    Added branch instructions

---
+++ brainstorm/Processors/SP2000/Instructions/Branch/BgeInstruction.cs
@@ -0,0 +1,46 @@
+﻿using BrainStorm.Processors.SP2000.Instructions;
+using System;
+using BrainStorm.Processors.SP2000.Processor;
+using BrainStorm.Processors.SP2000.Memory;
+using brainstorm.Base;
+using brainstorm.Exceptions;
+
+namespace brainstorm.Processors.SP2000.Instructions.Branch
+{
+    class BgeInstruction : ThreeOperandInstruction
+    {
+        private string firstRegister;
+        private string secondRegister;
+        private int address;
+        public BgeInstruction(string instruction, string firstRegister, string secondRegister, int address) : base(instruction, firstRegister, secondRegister, address.ToString())
+        {
+            this.firstRegister = firstRegister;
+            this.secondRegister = secondRegister;
+            this.address = address;
+        }
+
+        public override void execute(SP2000Core core)
+        {
+            SP2000Registers registers = (SP2000Registers)core.Registers;
+
+            try
+            {
+                Register first = registers.FetchRegister(this.firstRegister);
+                Register second = registers.FetchRegister(this.secondRegister);
+
+                if (first.GetValue() >= second.GetValue())
+                {
+                    Register pc = registers.FetchRegister("PC");
+                    pc.SetValue(this.address);
+                }
+            }
+            catch (RegisterException e)
+            {
+                Console.WriteLine(e.Message);
+            }finally
+            {
+                core.Cycles += Cycles;
+            }
+        }
+    }
+}
---
+++ brainstorm/Processors/SP2000/Instructions/Branch/BleInstruction.cs
@@ -0,0 +1,47 @@
+﻿using BrainStorm.Processors.SP2000.Instructions;
+using BrainStorm.Processors.SP2000.Processor;
+using BrainStorm.Processors.SP2000.Memory;
+using brainstorm.Base;
+using brainstorm.Exceptions;
+using System;
+
+namespace brainstorm.Processors.SP2000.Instructions.Branch
+{
+    class BleInstruction : ThreeOperandInstruction
+    {
+        private string firstRegister;
+        private string secondRegister;
+        private int address;
+        public BleInstruction(string instruction, string firstRegister, string secondRegister, int address) : base(instruction, firstRegister, secondRegister, address.ToString())
+        {
+            this.firstRegister = firstRegister;
+            this.secondRegister = secondRegister;
+            this.address = address;
+        }
+
+        public override void execute(SP2000Core core)
+        {
+            SP2000Registers registers = (SP2000Registers)core.Registers;
+
+            try
+            {
+                Register first = registers.FetchRegister(this.firstRegister);
+                Register second = registers.FetchRegister(this.secondRegister);
+
+                if (first.GetValue() <= second.GetValue())
+                {
+                    Register pc = registers.FetchRegister("PC");
+                    pc.SetValue(this.address);
+                }
+            }
+            catch (RegisterException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                core.Cycles += Cycles;
+            }
+        }
+    }
+}
