4b5fd191cde157b7fa744998b0b47ddc1620fd1b
Kevin Nderitu <nderitukelvin19@gmail.com>
2017-01-22 13:56:12 +0300

    Test manenos

--- BrainStorm/Test.cs
+++ BrainStorm/Test.cs
@@ -7,36 +7,39 @@ using BrainStorm.Processors.SP2000.Instructions;
 using BrainStorm.Base;
 using BrainStorm.Processors.SP2000.Instructions.Branch;
 using BrainStorm.Processors.SP2000.Instructions.Jump;
+using BrainStorm.Simulators;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace BrainStorm
 {
-
     class Test
     {
         public static void Main(string[] args)
         {
             
-                SP200InstructionMemory program = new SP200InstructionMemory();
+                SP2000InstructionMemory program = new SP2000InstructionMemory();
                 SP2000DataMemory memory = new SP2000DataMemory();
                 //JalInstruction li = new JalInstruction("jal 4", 40);
-                LaInstruction lia = new LaInstruction("li $v0, 3", "$v0", 4);
-                //JrInstruction j = new JrInstruction("jr $ra", "$v0");
-                //BeqInstruction lw = new BeqInstruction("blt $v1, $v0, 0", "$v1", "$v0", 20);
+                LaInstruction li = new LaInstruction("li $t0, 4", "$t0", 4);
+            LaInstruction li2 = new LaInstruction("li $t1, 5", "$t1", 5);
+            LaInstruction li3 = new LaInstruction("li $t2, 6", "$t2", 6);
+            LaInstruction li4 = new LaInstruction("li $t3, 7", "$t3", 7);
+            LaInstruction li5 = new LaInstruction("li $t0, 8", "$t4", 8);
+            //BeqInstruction lw = new BeqInstruction("blt $v1, $v0, 0", "$v1", "$v0", 20);
 
-                program.Push(lia, 0);
-                //program.Push(li, 1);
-                //program.Push(j, 1);
+            program.Push(li, 0);
+            program.Push(li2, 1);
+            program.Push(li3, 2);
+            program.Push(li4, 3);
+            program.Push(li5, 4);
+            //program.Push(j, 1);
 
-                SP2000Processor chip = new SP2000Processor(program);
+            //SP2000Simulator mips = new SP2000Simulator(0, program, 5);
+            // mips.Run();
 
-                //chip.Tick();
-                //chip.Tick();
-                chip.Tick();
-                chip.ShowRegisters();
-
-
-            SP2000DataMemory mem = SP2000DataMemory.Instance;
-            mem.ShowMemory();
+            // SP2000DataMemory mem = SP2000DataMemory.Instance;
+            // mem.ShowMemory();
 
             /*  int value = 1088;
 
@@ -45,7 +48,12 @@ namespace BrainStorm
              int res = (int)(value & 0xFFFF0000);
 
              Convert(res); */
-        
+            string file = @"C:\Users\Nderitu\Desktop\emulation\mips.asm";
+            SP2000Assembler assembler = new SP2000Assembler(file);
+            assembler.Assemble();
+
+            SP2000DataMemory.Instance.ShowMemory();
+
         }
         public static void Convert(int num)
         {
@@ -62,4 +70,5 @@ namespace BrainStorm
             Console.WriteLine("");
         }
     }
+   
 }
