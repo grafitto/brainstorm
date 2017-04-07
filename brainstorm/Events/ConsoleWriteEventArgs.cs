d802259bb2f19087f0e7ad50d1630da5b0d74436
Kevin Nderitu <nderitukelvin19@gmail.com>
2017-04-07 21:20:05 +0300

    Event arg classes for I/O events

---
+++ brainstorm/Events/ConsoleWriteEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainStorm.Events
+{
+    public class ConsoleWriteEventArgs : EventArgs
+    {
+        public string Message { get; }
+        public ConsoleWriteEventArgs(string message)
+        {
+            Message = message;
+        }
+    }
+}
